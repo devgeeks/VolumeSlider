6589f65f609f7665e4a9495a23c1eec87db2ccd9
Samuel Michelot <sam.pub@gmail.com>
2013-11-05 11:19:59 -0500

    Fix bug and use better syntax

--- www/VolumeSlider.js
+++ www/VolumeSlider.js
@@ -6,41 +6,29 @@
 //	Copyright 2011 Tommy-Carlos Williams. All rights reserved.
 //      MIT Licensed
 //
-var VolumeSlider = function () {
 
-};
+var exec = require('cordova/exec');
 
-VolumeSlider.prototype = {
 
-    clear: function () {
-        cordova.exec(null, null, 'Badge', 'setBadge', [0]);
-    },
+module.exports = {
 
-    set: function (badge) {
-        cordova.exec(null, null, 'Badge', 'setBadge', [parseInt(badge) || 0]);
-    }
-
-	/**
+    /**
 	 * Create a volume slider.
 	 */
 	createVolumeSlider : function(originx,originy,width,height) {
-		cordova.exec(null, null, "VolumeSlider","createVolumeSlider", [originx, originy, width, height]);
-	};
+		exec(null, null, "VolumeSlider","createVolumeSlider", [originx, originy, width, height]);
+	},
 	
 	/**
 	 * Show the volume slider
 	 */
 	showVolumeSlider : function() {
-		cordova.exec(null, null, "VolumeSlider","showVolumeSlider", []);
-	};
+		exec(null, null, "VolumeSlider","showVolumeSlider", []);
+	},
 	/**
 	 * Hide the volume slider
 	 */
 	hideVolumeSlider : function() {
-		cordova.exec(null, null, "VolumeSlider","hideVolumeSlider", []);
-	};
+		exec(null, null, "VolumeSlider","hideVolumeSlider", []);
+	}
 };
-
-var plugin = new VolumeSlider();
-
-module.exports = plugin;
